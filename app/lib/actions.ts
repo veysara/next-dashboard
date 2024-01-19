d0e3adc43575e7783b286811bd0fdb20c83fcde1
Sara VEY <vey.sara@optistech.com>
2024-01-19 17:05:18 +0700

    improve

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,7 +72,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   throw new Error('Failed to Delete Invoice');
  
-  // Unreachable code block
+  // Unreachable code block       
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
